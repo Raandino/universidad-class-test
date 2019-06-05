b97d569f1f3921cb26c0c307ef7730a1f89d5c5a
Raandino <aandino186@gmail.com>
2019-06-04 22:30:37 -0600

    byr

--- admin/main/validacioncontra.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$usuario = $_SESSION['usuario'];
-$q = "SELECT COUNT(*) as cambiar from login where usuario = '$usuario' and clave = '12345678'";
-
-$consulta = mysqli_query($conexion, $q);
-$array = mysqli_fetch_array($consulta);
-
-?>
